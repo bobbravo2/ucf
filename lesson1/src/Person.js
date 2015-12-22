401ab055a76d9ffd56177170268027f68975b299
Robert Gregor <bob@orionipventures.com>
2015-12-22 13:13:42 -0500

    refactored to use private methods for validation

--- lesson1/src/Person.js
+++ lesson1/src/Person.js
@@ -3,20 +3,22 @@
  */
 
 function Person(name, email) {
-    this.name = name;
-    if ('undefined' == typeof(name)) {
-        throw 'Name is required';
-    }
-    //Unicode compatible email regex from: http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
-    var emailRegEx = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    if (! emailRegEx.test(email)) {
-        //TODO validate on server side as well! JS can be disabled.
-        throw 'Invalid Email';
-    }
-    this.email = email;
+    this.validateName = function (name) {
+        if ('undefined' == typeof(name)) {
+            throw 'Name is required';
+        }
+        return name;
+    };
+    this.validateEmail = function (email) {
+        //Unicode compatible email regex from: http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+        var emailRegEx = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (! emailRegEx.test(email)) {
+            //TODO validate on server side as well! JS can be disabled.
+            throw 'Invalid Email';
+        }
+        return email;
+    };
+
+    this.name = this.validateName(name);
+    this.email = this.validateEmail(email);
 }
-
-
-
-
-
