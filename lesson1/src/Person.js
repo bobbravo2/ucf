9efe519e4b1ae830a80c801def5b1b51ea0851ca
Robert Gregor <bob@orionipventures.com>
2015-12-22 13:10:42 -0500

    passing tests & cs cleanup

--- lesson1/src/Person.js
+++ lesson1/src/Person.js
@@ -3,17 +3,17 @@
  */
 
 function Person(name, email) {
-	this.name = name;
-	if ('undefined' == typeof(name)) {
-		throw 'Name is required';
-	}
-	//Unicode compatible email regex from: http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
-	var emailRegEx = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-	if (! emailRegEx.test(email)) {
-		//TODO validate on server side as well! JS can be disabled.
-		throw 'Invalid Email';
-	}
-	this.email = email;
+    this.name = name;
+    if ('undefined' == typeof(name)) {
+        throw 'Name is required';
+    }
+    //Unicode compatible email regex from: http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+    var emailRegEx = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (! emailRegEx.test(email)) {
+        //TODO validate on server side as well! JS can be disabled.
+        throw 'Invalid Email';
+    }
+    this.email = email;
 }
 
 
--- lesson1/test/PersonTest.js
+++ lesson1/test/PersonTest.js
@@ -4,8 +4,8 @@
 TestCase("PersonTest", {
 	"test valid constructor":   function () {
 		var name  = 'Bob',
-				email = 'over40email@aol.com';
-		var Bob = new Person(name, email);
+            email = 'over40email@aol.com',
+            Bob = new Person(name, email);
 		assertEquals('Error setting name', name, Bob.name);
 		assertEquals('Error setting email', email, Bob.email);
 	},
