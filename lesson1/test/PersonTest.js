5a5a230ffdbab8dfb06f011d4bbb25ce838206c3
Robert Gregor <bob@orionipventures.com>
2015-12-22 13:02:59 -0500

    added test for invalid email exceptions

--- lesson1/test/PersonTest.js
+++ lesson1/test/PersonTest.js
@@ -2,11 +2,18 @@
  * Created by robertgregor on 12/22/15.
  */
 TestCase("PersonTest", {
-	"test valid constructor" : function() {
-		var name = 'Bob',
+	"test valid constructor": function () {
+		var name  = 'Bob',
 				email = 'over40email@aol.com';
 		var Bob = new Person(name, email);
 		assertEquals('Error setting name', name, Bob.name);
 		assertEquals('Error setting email', email, Bob.email);
+	},
+	"test invalid email":     function () {
+		var name  = 'Bob2',
+				email = 'foobar';
+		assertException('Invalid email did not throw exception',function () {
+			new Person(name, email)
+		});
 	}
 });
