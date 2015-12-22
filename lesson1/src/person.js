c51f9212c28c0039716e56a7e8f496c5a7a6e3d7
Robert Gregor <bob@orionipventures.com>
2015-12-22 13:04:15 -0500

    added person.js & ide settings

--- lesson1/src/person.js
+++ lesson1/src/person.js
@@ -2,10 +2,16 @@
  * Created by robertgregor on 12/22/15.
  */
 
-
-
 function Person(name, email) {
 	this.name = name;
+	//Unicode compatible email regex from: http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+	//TODO validate on server side as well! JS can be disabled.
+	var emailRegEx = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+	if (! emailRegEx.test(email)) {
+		throw 'Invalid Email';
+	}
 	this.email = email;
 }
+
+
 
